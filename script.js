ed0248a80723a8f0d9945ab437a1d32985554817
slimecoding2025 <138517613+slimecoding2025@users.noreply.github.com>
2025-03-06 01:06:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function checkPassword() {
     const password = document.getElementById("password").value;
-    const correctPassword = "05012007";
-
-    if (password === correctPassword) {
-        document.getElementById("passwordScreen").classList.add("hidden");
-        document.getElementById("content").classList.remove("hidden");
+    if (password === "05012007") {
+        document.querySelector(".password-box").classList.add("fade-out");
+        setTimeout(() => {
+            document.querySelector(".password-box").style.display = "none";
+            document.getElementById("content").classList.remove("hidden");
+            document.getElementById("content").classList.add("fade-in");
+        }, 1000);
     } else {
-        alert("Incorrect password. Please try again.");
+        alert("Incorrect Password!");
     }
 }
